efb576426e3a809f788f52281fe62f3a6aaf8d57
Akhileshwar Padala <apadala1@kent.edu>
2018-09-10 15:55:16 -0400

    bug fix on Set methods

--- ArraysStrings/src/main/java/com/apadala/ArraysStrings/App.java
+++ ArraysStrings/src/main/java/com/apadala/ArraysStrings/App.java
@@ -6,20 +6,22 @@ package com.apadala.ArraysStrings;
  */
 public class App {
 	public static void main(String[] args) {
-        long lStartTime = System.nanoTime();
+		long lStartTime = System.nanoTime();
+		String input="abcdefghijklmnopqrstuvwxyza";
 
-		System.out.println("strrting string testing ............");
-		System.out.println("Passing String values...............");
-		
-		System.out.println(new StringUniqueChars().StringTestncs("AkHil"));
-		
+		System.out.println(new StringUniqueChars().StringTest(input));
+		System.out.println(new StringUniqueChars().StringTestncs(input));
+		System.out.println(new StringUniqueChars().StringTestSet(input));
+		System.out.println(new StringUniqueChars().StringTestSetncs(input));
+		System.out.println(new StringUniqueChars().StringTestBln(input));
+		//System.out.println(new StringUniqueChars().StringTestSetncs(input));
+
 		long lEndTime = System.nanoTime();
 
-		//time elapsed
-        long output = lEndTime - lStartTime;
+		// time elapsed
+		long output = lEndTime - lStartTime;
 
-        System.out.println("Elapsed time in milliseconds: " + output / 1000000);
+		System.out.println("Elapsed time in milliseconds: " + output / 1000000);
 
-		
 	}
 }
--- ArraysStrings/src/main/java/com/apadala/ArraysStrings/StringUniqueChars.java
+++ ArraysStrings/src/main/java/com/apadala/ArraysStrings/StringUniqueChars.java
@@ -14,8 +14,8 @@ import java.util.Set;
 public class StringUniqueChars {
 
 	/**
-	 * This method implementation only uses String APi , No other Data Structures, Case Sensitive 
-	 * CASE sensitive Retrun true if all unique
+	 * This method implementation only uses String APi , No other Data Structures,
+	 * Case Sensitive CASE sensitive Retrun true if all unique
 	 * 
 	 * @param input
 	 * @return
@@ -40,9 +40,10 @@ public class StringUniqueChars {
 		return !flag;
 
 	}
+
 	/**
-	 * This method implementation only uses String APi , No other Data Structures NoT Case Sensitive 
-	 * CASE sensitive Retrun true if all unique
+	 * This method implementation only uses String APi , No other Data Structures
+	 * NoT Case Sensitive CASE sensitive Retrun true if all unique
 	 * 
 	 * @param input
 	 * @return
@@ -50,15 +51,15 @@ public class StringUniqueChars {
 
 	boolean StringTestncs(String input) {
 		boolean flag = false;
-		String temp1,temp2;
+		String temp1, temp2;
 		char ch;
 		int i = 0;
 		while (!flag && i < input.length()) {
 			int j = 0;
 			while (!flag && j < input.length()) {
 				if (i != j) {
-					flag = input.toLowerCase().charAt(i)==input.toLowerCase().charAt(j);
-					
+					flag = input.toLowerCase().charAt(i) == input.toLowerCase().charAt(j);
+
 				}
 				j++;
 
@@ -71,28 +72,36 @@ public class StringUniqueChars {
 	}
 
 	/**
-	 * method implementation uses Set interface to find the if String has All unique , Case Sensitive 
-	 * CASE sensitive Characters Retrun true If All unique
+	 * method implementation uses Set interface to find the if String has All unique
+	 * , Case Sensitive CASE sensitive Characters Retrun true If All unique
 	 * 
 	 * @param input
 	 * @return
 	 */
 	boolean StringTestSet(String input) {
 		boolean flag = true;
-		Set s1 = new HashSet();
+		Set<Character> s1 = new HashSet<Character>();
 		int i = 0;
-		while ((flag = s1.add(input.charAt(i))) && i < input.length()) {
-			i++;
+		try {
+			while (i < input.length() && (flag = s1.add(input.charAt(i)))) {
+
+				// System.out.println(input.charAt(i)+"===="+i);
+				// System.out.println("================");
+				i++;
+
+			}
+		} catch (StringIndexOutOfBoundsException e) {
+			System.out.println(e);
 
 		}
-		System.out.println(s1);
+		// System.out.println(s1);
 
 		return flag;
 	}
-	
+
 	/**
-	 * method implementation uses Set interface to find the if String has All unique ,NOT  Case Sensitive 
-	 * CASE sensitive Characters Retrun true If All unique
+	 * method implementation uses Set interface to find the if String has All unique
+	 * ,NOT Case Sensitive CASE sensitive Characters Retrun true If All unique
 	 * 
 	 * @param input
 	 * @return
@@ -100,15 +109,34 @@ public class StringUniqueChars {
 
 	boolean StringTestSetncs(String input) {
 		boolean flag = true;
-		Set s1 = new HashSet();
+		Set<Character> s1 = new HashSet<Character>();
 		int i = 0;
-		while ((flag = s1.add(input.toLowerCase().charAt(i))) && i < input.length()) {
+		while ((i < input.length() && (flag = s1.add(input.toLowerCase().charAt(i))))) {
 			i++;
 
 		}
-		System.out.println(s1);
+		//System.out.println(s1);
 
 		return flag;
+	}
+
+	/***
+	 * Method Uses boolean array to find the answer
+	 * 
+	 * @param str
+	 * @return
+	 */
+
+	public static boolean StringTestBln(String input) {
+		boolean[] char_set = new boolean[256];
+
+		for (int i = 0; i < input.length(); i++) {
+
+			if (char_set[(int) (input.charAt(i))])
+				return false;
+			char_set[(int) (input.charAt(i))] = true;
+		}
+		return true;
 	}
 
 }
