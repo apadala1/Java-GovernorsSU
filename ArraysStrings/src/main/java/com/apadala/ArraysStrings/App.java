8584929b342d03016b7973003bef6accff59ec3b
Akhileshwar Padala <apadala1@kent.edu>
2018-09-10 14:47:08 -0400

    If String Has All Unique Characters

--- ArraysStrings/src/main/java/com/apadala/ArraysStrings/App.java
+++ ArraysStrings/src/main/java/com/apadala/ArraysStrings/App.java
@@ -4,10 +4,22 @@ package com.apadala.ArraysStrings;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+        long lStartTime = System.nanoTime();
+
+		System.out.println("strrting string testing ............");
+		System.out.println("Passing String values...............");
+		
+		System.out.println(new StringUniqueChars().StringTestncs("AkHil"));
+		
+		long lEndTime = System.nanoTime();
+
+		//time elapsed
+        long output = lEndTime - lStartTime;
+
+        System.out.println("Elapsed time in milliseconds: " + output / 1000000);
+
+		
+	}
 }
