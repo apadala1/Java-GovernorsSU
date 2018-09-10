a598ed4e60fc8f1285ec4fa8d2f78a883a1be846
Akhileshwar Padala <apadala1@kent.edu>
2018-09-10 12:56:11 -0400

    project created

---
+++ ArraysStrings/src/main/java/com/apadala/ArraysStrings/App.java
@@ -0,0 +1,13 @@
+package com.apadala.ArraysStrings;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
