af218c0461b4ccdb369572998c40fc4ca872fedb
Akhileshwar Padala <apadala1@kent.edu>
2018-09-19 16:47:56 -0400

    magic square

---
+++ ArraysStrings/src/main/java/com/apadala/ArraysStrings/MagicSquare.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.apadala.ArraysStrings;
+
+/**
+ * @author Akhileshwar.Padala
+ *
+ */
+public class MagicSquare {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
